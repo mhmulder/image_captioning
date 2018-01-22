d129354a90c63b669af379055609e0fa9c5c9c74
Michael Mulder <michaelmulder@Michaels-MacBook-Pro.local>
2018-01-22 10:17:34 -0700

    more docstrings

---
+++ src/database_string_cleaning_step_4/database_string_cleaning.py
@@ -0,0 +1,206 @@
+import numpy as np
+import pandas as pd
+from pymongo import MongoClient
+import re
+from string import punctuation
+from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
+import os
+from collections import Counter
+
+'''
+This script does string processing and gets the text ready for analysis.
+'''
+
+
+def count_dict(list_of_strings):
+    """
+    Creates a counter dictionary, that counts each word in the corpus of
+    descriptions.
+
+    paramters:
+    --------------------------
+    list_of_strings (list) -> list of descriptions
+
+    returns:
+    --------------------------
+    my_counter (dict) -> A dictionary with words and their corresponding counts
+    """
+    PUNCT_DICT = {ord(punc): None for punc in punctuation}
+    string = " ".join(list_of_strings)
+    string = string.translate(PUNCT_DICT).lower()
+    tokens = string.strip(" ").split(" ")
+
+    my_counter = Counter(tokens)
+
+    return my_counter
+
+
+def process_string(string, count_dict):
+    """
+    Cleaning function, that strips punctuation, makes everything lowercase,
+    adds a '<end>' and '<start>' token, removes tokens that start with numbers,
+    removes stopwords, removes words less than 3 characters, removes words that
+    appear fewer than 5 times.
+
+    paramters:
+    --------------------------
+    string (str) -> A string to process
+
+    count_dict (dict) -> A dictionary with words and their corresponding counts
+
+    returns:
+    --------------------------
+    (str) -> A cleaned and processed string
+    """
+    PUNCT_DICT = {ord(punc): None for punc in punctuation}
+    string = string.translate(PUNCT_DICT).lower()
+    tokens = string.strip(" ").split(" ")
+
+    clean_words = ['<start>']
+    for word in tokens:
+        if len(word) < 3:
+            continue
+        # regex remove words starting with numbers
+        matches = re.match('\d', word)
+        if matches:
+            continue
+        if count_dict[word] < 5:
+            continue
+        clean_words.append(word)
+    clean_words.append('<end>')
+
+    # can add additional stop words here
+    stopwords = set(list(ENGLISH_STOP_WORDS) + ['deep', 'high', 'wide'])
+
+    return ' '.join(word for word in clean_words if word not in stopwords)
+
+
+def run_query(query, collection):
+    """
+    Queries a Mongo collection for something and returns the results.
+
+    paramters:
+    --------------------------
+    query (str) -> A query to run through MongoDB
+
+    collection (MongoDB collection) -> A collection containing the
+    information for all product ID's
+
+    returns:
+    --------------------------
+    (MongoDB cursor) -> A cursor containing the results of the query
+    """
+    rgx = re.compile(query, re.IGNORECASE)
+    results = collection.find({'$and': [{'categoryPath': rgx},
+                              {'imageEntities.thumbnailImage':
+                              {'$exists': True}}]})
+    return results
+
+
+def df_cleaning_func(df, count_dict):
+    """
+    Really simple df cleaning function, removes brand names from descriptions,
+    and then runs the process string function.
+
+    paramters:
+    --------------------------
+    df (pandas DataFrame) -> A dataframe with strings to process
+
+    count_dict (dict) -> A dictionary with words and their corresponding counts
+
+    returns:
+    --------------------------
+    df (pandas DataFrame) -> A dataframe with processed strings
+    """
+    # currently short may add_more
+    df.shortDescription = df['shortDescription'].str.strip('&lt;p&gt;').head()
+
+    # remove brand from name
+    name_striped = [name.replace(brand, '') for name, brand in zip(
+                    df['name'].astype('str'), df['brandName'].astype('str'))]
+
+    # function to clean strings
+    clean_striped = [process_string(desc, count_dict) for desc in name_striped]
+
+    # add column back to df
+    df['cleanDescription'] = clean_striped
+
+    return df
+
+
+def caption_mapping(filename, images_dir, item_desc_dict):
+    """
+    This function creates a csv with image names and their descriptions.
+    This document essentially links everything together.
+
+    paramters:
+    --------------------------
+    filename (str) -> The name or path of the resulting csv
+
+    images_dir (list) -> A list of all image names
+
+    item_desc_dict(dict) => A dictionary of descriptions and the image names
+
+    returns:
+    --------------------------
+    None, a csv is created in the directory associated with the filename
+    """
+    with open(filename, 'w') as w:
+        for pic_name in images_dir:
+            itemId = pic_name.split("_")[0]
+            try:
+                desc = item_desc_dict[int(itemId)]
+            except:
+                continue
+            w.write(pic_name + "," + desc + "\n")
+            w.flush()
+
+if __name__ == '__main__':
+    # load mongo client in the cloud
+    mongo_password = os.environ['capstone_mongo']
+    client = MongoClient(mongo_password)
+    db = client.capstone_project
+    collection = db.product_id_returns
+    print('Mongol loaded!')
+
+    # query mongo for all furniture items and return all items with images
+    results = run_query('HOME/Furniture.*', collection)
+    print('{} results found'.format(results.count()))
+
+    # convert data into a pandas dataframe
+    df = pd.DataFrame(list(results))
+    print('DataFrame created!')
+    list_of_strings = list(df['name'].values)
+
+    my_counter = count_dict(list_of_strings)
+
+    # clean df
+    cleaned_df = df_cleaning_func(df, my_counter)
+    print('DataFrame cleaned!')
+    # put descriptions in a dictionary
+    item_desc_dict = pd.Series(cleaned_df['cleanDescription'].values,
+                               index=cleaned_df['itemId']).to_dict()
+    print('Dictionaries made!')
+    # get all image names
+    images_dir = os.listdir("../../data/pics/")
+    print('{} pictures found!'.format(len(images_dir)))
+    # create csv for image mapping later
+    caption_mapping('../../data/caption_mapping.txt',
+                    images_dir,
+                    item_desc_dict)
+
+    # load csv, unnesciasary to convert back but helpful to have the file
+    image_description_pair = pd.read_csv('../../data/caption_mapping.txt',
+                                         names=['image_name', 'description'])
+
+    # create train/test splits
+    id_pair_8k_train = image_description_pair.iloc[0:8000]
+    id_pair_8k_test = image_description_pair.iloc[8000:10000]
+    id_pair_full_train = image_description_pair.iloc[:250000]
+    id_pair_full_test = image_description_pair.iloc[250000:]
+
+    id_pair_8k_test.to_csv('../../data/image_description_pair_8k_test.csv')
+    id_pair_8k_train.to_csv('../../data/image_description_pair_8k_train.csv')
+    id_pair_full_train.to_csv('../../data/image_description_full_train.csv')
+    id_pair_full_test.to_csv('../../data/image_description_full_test.csv')
+    print('Finished')
---
+++ src/image_scraping_step_3/image_scraper.py
@@ -0,0 +1,127 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import numpy as np
+import pandas as pd
+import re
+import time
+import csv
+import os
+import json
+import urllib
+import pickle
+import pymongo
+from pymongo import MongoClient
+import pprint
+from bson import json_util
+import json
+import pickle
+import pprint
+from collections import defaultdict
+import sys
+import glob
+import alert_program
+import boto3
+
+def run_query(collection):
+    rgx = re.compile('HOME/Furniture.*', re.IGNORECASE)
+    new_results = collection.find({'$and': [{'categoryPath': rgx}, {'imageEntities.thumbnailImage' : {'$exists': True}}]},
+                                    {'itemId': 1,
+                                     'categoryPath' : 1,
+                                     'imageEntities.thumbnailImage' : 1
+                                    })
+    return new_results
+
+def append_to_csv(filepath, old_list, new_list):
+    new_img_list = list(set(old_list + new_list))
+
+    with open(filepath, 'w', newline='') as myfile:
+        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        wr.writerow(new_img_list)
+    print("File saved as {}".format(filepath))
+
+def read_csv(filepath):
+    with open (filepath, 'r') as mycsv:
+        images = csv.reader(mycsv, delimiter = ',')
+        img_list = [img_num for img_num in images][0]
+    return img_list
+
+def create_img_dict(filepath, verbose=0):
+    img_list = read_csv(filepath)
+    # print(img_list)
+    img_dict = defaultdict(list)
+    high_num = 0
+    for elem in img_list:
+        # test = elem.strip('.jpg').split('_')
+        img_product, img_num = elem.strip('.jpg').split('_')
+        img_num = int(img_num)
+        img_dict[img_product].append(img_num)
+        if img_num > high_num:
+            high_num = img_num
+    if verbose != 0:
+        print ('Image_dict_created')
+    return img_dict, high_num, img_list
+
+
+def get_photos(results, filepath, start, end, csv_name):
+    img_dict, high_num, old_list = create_img_dict(csv_name)
+    img_num = 0
+    img_list = []
+    results = [result for result in results]
+    for i in range(start, end):
+        if i % 10 == 0:
+            print ('Now downloading pictures from product number {}/{}'.format(i, len(results)))
+            append_to_csv(filepath, old_list, img_list)
+        item_id = results[i]['itemId']
+        if str(item_id) in img_dict:
+            continue
+        for counter, item in enumerate(results[i]['imageEntities']):
+            # time.sleep(0.2)
+            img_num += 1
+            url = item['thumbnailImage']
+            filename = '{}_{:02}.jpg'.format(item_id,counter)
+            file_path = 'pics/{}'.format(filename)
+            try:
+                urllib.request.urlretrieve(url, file_path)
+            except:
+                continue
+            img_list.append(filename)
+    print ('{} total images in directory.'.format(img_num))
+    append_to_csv(filepath, old_list, img_list)
+
+def get_ids_from_directory():
+    print (os.getcwd())
+    img_list = []
+    files = glob.glob(os.getcwd() + "/pics/"+"*.jpg")
+    for fle in files:
+        clean = fle.split('/')[-1]
+        img_list.append(clean)
+    append_to_csv('pics/img_list.csv', img_list, img_list)
+    return (files)
+
+def download_csv_from_s3(s3_client, bucket_name, remote_filename, local_filename ):
+    # Download the file from S3
+    s3_client.download_file(bucket_name, remote_filename, local_filename)
+
+
+
+
+
+
+if __name__ == '__main__':
+    start = int(sys.argv[1])
+    end = int(sys.argv[2])
+    # s3_client = boto3.client('s3')
+    # download_csv_from_s3(s3_client, 'mm-capstone-images', 'pictures/img_list.csv', 'img_list.csv')
+
+    client = MongoClient("mongodb://mickey:mickeypassword@52.90.26.60/capstone_project")
+    db = client.capstone_project
+    collection = db.product_id_returns
+    results = run_query(collection)
+    # print (results[0])
+    try:
+        get_photos(results, 'pics/img_list.csv', start, end, 'img_list.csv')
+    except:
+        alert_program.send_end_alert('capstone', 'You have an error!')
+    # files = get_ids_from_directory()
+    # print (len(files))
